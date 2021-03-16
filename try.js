7d2df285ce83b68062851507db1d2bbbafc31b52
bong7233 <77820303+bong7233@users.noreply.github.com>
2021-03-16 14:33:44 +0900

    희소배열 내용추가

--- try.js
+++ try.js
@@ -1,13 +1,11 @@
+const arr = [1, 2, 3, 4, 5];
 
+// 현재 length 프로퍼티 값인 5보다 작은 숫자 값 3을 length 프로퍼티에 할당
+arr.length = 3;
 
-function sum(...args) {
-  var result = 0;
+// 배열의 길이가 5에서 3으로 줄어든다.
+console.log(arr); // [1, 2, 3]
 
-  console.log(args);
-  // for (var i = 0; i < arguments.length; i++)
-  //   result =+ arguments[i];
-  // }
-  return result;
-}
-
-console.log(sum(1,2,3,4));
+// 더 많은수인 6을할당
+arr.length = 6;
+console.log(arr); // [1,2,3, <3 empty items> ]
