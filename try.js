59853e1c82fc23aa21cc36930a20a9212f81d523
bong7233 <77820303+bong7233@users.noreply.github.com>
2021-03-17 15:19:53 +0900

    내용수정

--- try.js
+++ try.js
@@ -1,15 +1,10 @@
-const arr = [1, 2, 3, 4, 5];
+const sum = function (...args){
+    console.log(args);
+    let sum = 0;
+    for (let i = 0; i < args; i++){
+        sum += args[i];
+    }
+    return sum;
+}
 
-// 현재 length 프로퍼티 값인 5보다 작은 숫자 값 3을 length 프로퍼티에 할당
-arr.length = 3;
-
-// 배열의 길이가 5에서 3으로 줄어든다.
-console.log(arr); // [1, 2, 3]
-
-// 더 많은수인 6을할당
-arr.length = 6;
-console.log(arr); // [1,2,3, <3 empty items> ]
-
-
-const result = [1, 2, 3].forEach(console.log);
-console.log(result); // undefined
+sum(1,2,3);
